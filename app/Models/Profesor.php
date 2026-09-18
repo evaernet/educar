48f8429cfbe0d7cc760c7d3397646e4ce5cafa93
EnzoVaernet <enzo_vaernet@hotmail.com>
2026-09-18 20:47:39 -0300

    Completar modelo Profesor con fillable, casts y relacion con User

--- app/Models/Profesor.php
+++ app/Models/Profesor.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Profesor extends Model
 {
-    //
+    protected $fillable = [
+        'user_id','legajo','dni','nombre', 'apellido', 'email', 'telefono', 'activo', 'especialidad',
+    ];
+
+    protected function casts():array
+    {
+        return[
+            'activo' => 'boolean'
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
 }
